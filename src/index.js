9029d35cccfb4cc2e63e842bb8cb846973f37f73
LangerH <112247772+LangerH@users.noreply.github.com>
2022-12-01 21:56:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
